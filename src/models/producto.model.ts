9c58f1fac0d427b7b5d2d824fc13ba75134fbc23
Jeferson Arango L <jeferarlo2@gmail.com>
2021-09-06 18:14:57 -0500

    llaves foráneas de los modelos

--- src/models/producto.model.ts
+++ src/models/producto.model.ts
@@ -1,10 +1,21 @@
-import {Entity, model, property, belongsTo, hasMany} from '@loopback/repository';
-import {Marca} from './marca.model';
-import {Categoria} from './categoria.model';
+import {belongsTo, Entity, hasMany, model, property} from '@loopback/repository';
 import {CategoriaProducto} from './categoria-producto.model';
+import {Categoria} from './categoria.model';
 import {Imagen} from './imagen.model';
+import {Marca} from './marca.model';
 
-@model()
+@model({
+  settings: {
+    foreignKeys: {
+      fk_producto_id_marca: {
+        name: 'fk_producto_id_marca',
+        entity: 'Marca',
+        entityKey: 'id',
+        foreignKey: 'id_marca',
+      }
+    },
+  },
+})
 export class Producto extends Entity {
   @property({
     type: 'number',
